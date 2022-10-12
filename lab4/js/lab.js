ad5727aef17e570440487ea07f30fc3778f400eb
tvandixon <114507222+tvandixon@users.noreply.github.com>
2022-10-11 21:36:24 -0700

    lab 4

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,14 +1,15 @@
-// Summary. (use period)
-// Description. (use period)
-// @link   URL
-// @file   This files defines the MyClass class.
-// @author AuthorName.
-// @since  x.x.x
+// Pseudocode for creating a form.
+// For users to create an account on a website, they can fill this form.
+// @link   N/A
+// @file   This file defines the MyClass class.
+// @author Tristan Van Dixon, Jacquelyn Meek, Alex Jones.
+// @since  10.11.22
 
-// Print the board before each turn
-    // Loop through each row
-        // Loop through each space
-            // Print X, O, or Space
-            // Print a | between
-        // Print a newline at the end
-        // Print a horizontal line between
+// Print Log-in Page.
+    // Allow user input; email and password information.
+        // Determine if entered information correlates to security data.
+            // Accept entry if data correlates.
+            // Deny entry if data does not correlate.
+        // Allow user input; name, occupation, and biographical information.
+        //Prompt user for account setup confirmation.
+        //Print account creation success screen.
