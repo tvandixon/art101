eaec0badb0d458d11747a8faff717fefa48ee18c
tvandixon <114507222+tvandixon@users.noreply.github.com>
2022-10-24 15:19:59 -0700

    push to site

--- lab7/js/lab.js
+++ lab7/js/lab.js
@@ -1,5 +1,25 @@
 /*
- * Author:Alex Jones, Jacquelyn Meek, Tristan Van Dixon
+ * Author: Alex Jones, Jacquelyn Meek, Tristan Van Dixon
  * Created:   25.10.2023
  * License: Public Domain
  */
+
+// Username Function
+function scrambleUserName() {
+  var userName = window.prompt("Enter your name to get your certfied Frog identity:");
+  console.log("userName =", userName);
+  // Split userName letter-by-letter into an array.
+  var letterArray = userName.split("");
+  console.log("letterArray =", letterArray);
+  // Sort the letters within the array.
+  var letterArraySort = letterArray.sort();
+  console.log("letterArraySort =", letterArraySort)
+  // Put the letters back together scrambled.
+  var letterSorted = letterArraySort.join("");
+  console.log("letterSorted =". letterSorted);
+  // Return scrambled name.
+  return letterSorted;
+}
+
+// Output the new name on the site.
+document.writeln("You are now a frog named ", scrambleUserName(), ". </br>");
